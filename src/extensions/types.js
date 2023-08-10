ee0d2888eb1918a7a23d4d01a42f20b6c1332b8e
Ntina B <74467955+ntinab@users.noreply.github.com>
2023-08-10 18:33:38 +0300

    ok .

--- src/extensions/types.js
+++ src/extensions/types.js
@@ -1,12 +1,3 @@
-//
-// Variable types
-//
-// MDN: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#javascript_types
-// MDN: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
-//
-
-// Update the answers below the numbered questions below
-
 // An array of choices to pick answers from
 const CHOICES = [
   'Pick me!', // choose this with pick(0)
@@ -35,34 +26,34 @@ function pick(n) {
 }
 
 // 1. Pick true using the pick function - by changing 0 to pick your answer
-const imTrue = pick(0)
+const imTrue = pick(9) // Index 9 corresponds to the value '2 > 1' which is true
 
 // 2. Pick a real number
-const aReal = pick(0)
+const aReal = pick(4) // Index 4 corresponds to the value 3.14
 
 // 3. Pick a string
-const aString = pick(3)
+const aString = pick(6) // Index 6 corresponds to the value 'hey'
 
 // 4. Pick an array
-const anArray = pick(1)
+const anArray = pick(7) // Index 7 corresponds to the array [1, 2, 3]
 
 // 5. Pick a (simple) number
-const aNumber = pick(0)
+const aNumber = pick(3) // Index 3 corresponds to the value -12
 
 // 6. Pick an object
-const anObject = pick(1)
+const anObject = pick(8) // Index 8 corresponds to the object { i: 'object!' }
 
 // 7. Pick false
-const imFalse = pick(0)
+const imFalse = pick(10) // Index 10 corresponds to the value 3 === 4 which is false
 
 // 8. Pick a BigInt
-const imBigInt = pick(1)
+const imBigInt = pick(5) // Index 5 corresponds to the BigInt value 10000000n
 
 // 9. Pick undefined
-const imUndefined = pick(0)
+const imUndefined = pick(2) // Index 2 corresponds to the value undefined
 
 // 10. Pick null
-const imNull = pick(0)
+const imNull = pick(1) // Index 1 corresponds to the value null
 
 // Do not edit below this line
 module.exports = {
